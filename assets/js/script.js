fccf0ecbe7a8b442d71fba7da8dca8ea7849e602
Bewtaich <bewtaich@gmail.com>
2024-08-02 08:23:23 -0500

    collects task data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,63 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-const btn = document.querySelector('button');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     const rand = crypto.randomUUID();
-console.log(rand);
+    return rand
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    
+  const cardSkel=`<div class="card draggable">
+                 <div class="card-body">
+                  ${task.Title}, ${task.Date}, ${task.Desc}
+                 </div>
+                 </div>`;
+  $('#todo-cards').prepend(cardSkel)
 }
+
+
+const dialog = 
+$("#task-dialog-form").dialog({
+autoOpen: false,
+height: 400,
+width: 350,
+modal: true,
+buttons: {
+  "Add Task":handleAddTask,
+  Cancel: function() {
+    dialog.dialog("close");
+    }
+}
+})
+
+
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
 
-}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-    
+function handleAddTask(){
+  
+  const title = $('#taskTitle')
+  const date = $('#taskDate')
+  const desc = $('#taskDesc')
+  let currenttaskid = generateTaskId();
+  const task = {
+    Title:title.val(),
+    Date:date.val(),
+    Desc:desc.val(),
+    id:currenttaskid
+  }
+
+  console.log(task)
+  createTaskCard(task);
 }
 
-btn.addEventListener('click',handleAddTask);
+
+
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
 
@@ -36,10 +69,15 @@ function handleDrop(event, ui) {
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+
 $(document).ready(function () {
 
+$("#add-task-button").on("click", function() {
+    dialog.dialog("open");
+  });
 });
 
-const rand = crypto.randomUUID();
-console.log(rand);
-
+const form = dialog.find( "form" ).on( "submit", function( event ) {
+  event.preventDefault();
+  handleAddTask();
+});
